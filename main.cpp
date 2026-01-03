5980f50d44f76cfdb7e40f2d842cda5ee2221393
salima <salimalk@Mac.fritz.box>
2026-01-03 12:00:00 +0100

    Day 2: Implemented Add and List features

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ struct Book {
     string isbn;
 };
 
-vector<Book> library; //dynamic array to store books in memory during runtime
+vector<Book> library;//dynamic array to store books in memory during runtime
 
 void displayMenu();
 void addBook();
@@ -23,17 +23,23 @@ void listBooks();
 
 int main() {
     int choice;
-    cout <<"Library System Initialized..." << endl; 
     
-    //loop keeps program running until user exits
     do {
         displayMenu();
-        cout << "Enter your choice: ";
-        if (!(cin >> choice)) {
-            cout << "Invalid input. Please enter a number: " << endl;
-            cin.clear(); //reset the error flag on cin
-            cin.ignore(10000,'\n'); 
-            continue;
+        // Keeps looping until user enters 1, 2, or 3.
+        while (true) {
+            cout << "Enter your choice: ";
+            if (cin >> choice) {
+                if (choice >= 1 && choice <= 3) {
+                    break;
+                } else {
+                    cout << "Invalid option. Please choose: \n1. Add New Book \n2. List All Books \n3. Exit" << endl;
+                }
+            } else {
+                cout << "Invalid input. Please enter a number." << endl;
+                cin.clear();//reset the error flag on cin             
+                cin.ignore(10000, '\n');  
+            }
         }
 
         switch (choice) {
@@ -43,29 +49,59 @@ int main() {
             case 2: 
                 listBooks();
                 break;
-            case 3: cout << "Exiting system." << endl; 
+            case 3: 
+                cout << "Exiting system..." << endl; 
                 break;
-            default: cout << "Invalid choice. Please try again." << endl;
         }
     } while (choice != 3);
 
     return 0;
 }
 
-//displays the main menu options to the console
 void displayMenu() {
-    cout << "\nLibrary Management System" << endl;
+    cout << "Library Management System" << endl;
     cout << "1. Add New Book" << endl;
     cout << "2. List All Books" << endl;
     cout << "3. Exit" << endl;
 }
 
-//placeholder for adding books
 void addBook() {
-    cout << "\nAdd Book functionality coming soon." << endl;
+    Book newBook;
+    cout << "\nEnter Book ID: ";
+    
+    while (!(cin >> newBook.id)) {
+        cout << "Invalid ID. Please enter a number: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+    
+    cin.ignore(); //clearing buffer
+
+    cout << "Enter Book Title: ";
+    getline(cin, newBook.title);
+
+    cout << "Enter Author Name: ";
+    getline(cin, newBook.author);
+
+    cout << "Enter ISBN: ";
+    getline(cin, newBook.isbn);
+
+    library.push_back(newBook);
+    cout << "\nThe book '" << newBook.title << "' is added successfully to the library!" << endl;
 }
 
-//placeholder for listing books
 void listBooks() {
-    cout << "\nList Books functionality coming soon." << endl;
+    if (library.empty()) {
+        cout << "\nLibrary is empty. Add a book first!" << endl;
+        return;
+    }
+
+    cout << "\nCurrent Inventory:\n " << endl;
+    for (int i = 0; i < library.size(); i++) {
+    cout << "Book ID: " << library[i].id << endl;
+    cout << "Book title: " << library[i].title << endl;
+    cout << "Author: " << library[i].author << endl;
+    cout << "ISBN: " << library[i].isbn <<endl;
+    cout << endl;
+    }
 }
