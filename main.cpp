909a31a0ab343f681d954f2b84b0d66b25657fbd
salima <salimalk@Mac.fritz.box>
2026-01-12 17:08:42 +0100

    Day 4: Implemented Search Feature and Updated Documentation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
-/*Description: Console-based application to manage book inventory.
- Uses C++ vectors for dynamic storage and basic input validation.*/
+/* Project: Library Management System
+ * Description: Console-based application to manage book inventory.
+ * Features: Add Books, List All, and Search by ID.
+ */
 
 #include <iostream>
 #include <vector>
@@ -9,99 +11,129 @@ using namespace std;
 
 //represents a single book entity with details
 struct Book {
-    int id;
-    string title;
-    string author;
-    string isbn;
+    int id;
+    string title;
+    string author;
+    string isbn;
 };
-
-vector<Book> library;//dynamic array to store books in memory during runtime
+//dynamic array to store books in memory during runtime
+vector<Book> library;
 
 void displayMenu();
 void addBook();
 void listBooks();
+void searchBook(); //new function
 
 int main() {
-    int choice;
-    
-    do {
-        displayMenu();
-        // Keeps looping until user enters 1, 2, or 3.
-        while (true) {
-            cout << "Enter your choice: ";
-            if (cin >> choice) {
-                if (choice >= 1 && choice <= 3) {
-                    break;
-                } else {
-                    cout << "Invalid option. Please choose: \n1. Add New Book \n2. List All Books \n3. Exit" << endl;
-                }
-            } else {
-                cout << "Invalid input. Please enter a number." << endl;
-                cin.clear();//reset the error flag on cin             
-                cin.ignore(10000, '\n');  
-            }
-        }
+    int choice;
+    
+    do {
+        displayMenu();
+        // Keeps looping until user enters the right number
+        while (true) {
+            cout << "Enter your choice: ";
+            if (cin >> choice) {
+                if (choice >= 1 && choice <= 4) {
+                    break;
+                } else {
+                    cout << "Invalid option. Please choose: \n1. Add New Book \n2. List All Books \n3. Search for book \n4. Exit" << endl;
+                }
+            } else {
+                cout << "Invalid input. Please enter a number." << endl;
+                cin.clear();//reset the error flag on cin             
+                cin.ignore(10000, '\n');  
+            }
+        }
 
-        switch (choice) {
-            case 1: 
-                addBook();
-                break;
-            case 2: 
-                listBooks();
-                break;
-            case 3: 
-                cout << "Exiting system..." << endl; 
-                break;
-        }
-    } while (choice != 3);
+        switch (choice) {
+            case 1: 
+                addBook();
+                break;
+            case 2: 
+                listBooks();
+                break;
+            case 3:
+                searchBook(); // new case
+                break;
+            case 4: 
+                cout << "Exiting the system." << endl; 
+                break;
+        }
+    } while (choice != 4);
 
-    return 0;
+    return 0;
 }
 
 void displayMenu() {
-    cout << "Library Management System" << endl;
-    cout << "1. Add New Book" << endl;
-    cout << "2. List All Books" << endl;
-    cout << "3. Exit" << endl;
+    cout << "Library Management System" << endl;
+    cout << "1. Add New Book" << endl;
+    cout << "2. List All Books" << endl;
+    cout << "3. Search Book by ID" << endl;
+    cout << "4. Exit" << endl;
 }
 
 void addBook() {
-    Book newBook;
-    cout << "\nEnter Book ID: ";
-    
-    while (!(cin >> newBook.id)) {
-        cout << "Invalid ID. Please enter a number: ";
-        cin.clear();
-        cin.ignore(10000, '\n');
-    }
-    
-    cin.ignore(); //clearing buffer
+    Book newBook;
+    cout << "\nEnter Book ID: ";
+    
+    while (!(cin >> newBook.id)) {
+        cout << "Invalid ID. Please enter a number: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
+    }
+    
+    cin.ignore(); //clearing buffer
 
-    cout << "Enter Book Title: ";
-    getline(cin, newBook.title);
+    cout << "Enter Book Title: ";
+    getline(cin, newBook.title);
 
-    cout << "Enter Author Name: ";
-    getline(cin, newBook.author);
+    cout << "Enter Author Name: ";
+    getline(cin, newBook.author);
 
-    cout << "Enter ISBN: ";
-    getline(cin, newBook.isbn);
+    cout << "Enter ISBN: ";
+    getline(cin, newBook.isbn);
 
-    library.push_back(newBook);
-    cout << "\nThe book '" << newBook.title << "' is added successfully to the library!" << endl;
+    library.push_back(newBook);
+    cout << "Book is added successfully to the library!\n" << endl;
 }
 
 void listBooks() {
-    if (library.empty()) {
-        cout << "\nLibrary is empty. Add a book first!" << endl;
-        return;
-    }
+    if (library.empty()) {
+        cout << "\nLibrary is empty. Add a book first!" << endl;
+        return;
+    }
 
-    cout << "\nCurrent Inventory:\n " << endl;
-    for (int i = 0; i < library.size(); i++) {
-    cout << "Book ID: " << library[i].id << endl;
-    cout << "Book title: " << library[i].title << endl;
-    cout << "Author: " << library[i].author << endl;
-    cout << "ISBN: " << library[i].isbn <<endl;
-    cout << endl;
-    }
+    cout << "\nCurrent Inventory:\n " << endl;
+    for (int i = 0; i < library.size(); i++) {
+    cout << "Book ID: " << library[i].id 
+        << " | title: " << library[i].title 
+        << " | Author: " << library[i].author 
+        << " | ISBN: " << library[i].isbn <<endl;
+    cout << endl;
+    }
+}
+// Performs a linear search to find a specific book by its ID
+void searchBook() {
+    int searchID;
+    cout << "\nEnter Book ID to search: ";
+    cin >> searchID;
+
+    bool found = false;
+    //loop to traverse the library vector efficiently
+    for (const auto& book : library) {
+        if (book.id == searchID) {
+            cout << "Book Found!" << endl;
+            cout << "ID: " << book.id << endl;
+            cout << "Title: " << book.title << endl;
+            cout << "Author: " << book.author << endl;
+            cout << "ISBN: " << book.isbn << endl;
+            cout << "-------------------------" << endl;
+            found = true;
+            break; //Exit loop immediately once found to save processing time
+        }
+    }
+    //handle the edge case where the ID does not exist
+    if (!found) {
+        cout << "Book with ID " << searchID << " not found.\n" << endl;
+    }
 }
